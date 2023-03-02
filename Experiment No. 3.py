36d7066932fa47e1d15e49579df47d4de3210611
danapelagio <124846908+danapelagio@users.noreply.github.com>
2023-03-02 15:34:09 +0800

    Add files via upload

---
+++ Experiment No. 3.py
@@ -0,0 +1,20 @@
+import random
+
+# generate a random number between 1 and 100
+number = random.randint(1, 100)
+
+# give the user 10 chances to guess the number
+for i in range(10):
+    guess = int(input("Guess the number (between 1 and 100): "))
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print("Congratulations, you guessed it!")
+        print("Number of guesses:", i+1)
+        break
+
+# if the user didn't guess the number, reveal it
+if guess != number:
+    print("Sorry, you ran out of guesses. The number was", number)
