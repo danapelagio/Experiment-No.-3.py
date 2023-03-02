cb872dd6a609f4cb321acc8adf2e32372d75aa35
danapelagio <124846908+danapelagio@users.noreply.github.com>
2023-03-02 15:37:48 +0800

    Update Experiment No. 3.py

--- Experiment No. 3.py
+++ Experiment No. 3.py
@@ -2,7 +2,6 @@ import random
 
 # generate a random number between 1 and 100
 number = random.randint(1, 100)
-
 # give the user 10 chances to guess the number
 for i in range(10):
     guess = int(input("Guess the number (between 1 and 100): "))
@@ -14,7 +13,6 @@ for i in range(10):
         print("Congratulations, you guessed it!")
         print("Number of guesses:", i+1)
         break
-
 # if the user didn't guess the number, reveal it
 if guess != number:
     print("Sorry, you ran out of guesses. The number was", number)
